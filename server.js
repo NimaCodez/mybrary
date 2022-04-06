2267808ee6bc2420b9021257db3ab41dc3a5f8cc
Nima <nimarahmati8par@gmail.com>
2022-04-06 11:03:17 +0430

    Initial Add Author and Search

--- server.js
+++ server.js
@@ -5,8 +5,10 @@ if (process.env.NODE_ENV !== 'production') {
 const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
+const bodyParser = require('body-parser')
 
 const indexRouter = require('./routes/index')
+const authorsRouter = require('./routes/author')
 
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
@@ -14,6 +16,7 @@ app.set('layout', 'layouts/layout')
 
 app.use(expressLayouts)
 app.use(express.static("public"))
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }))
 
 const mongoose = require('mongoose')
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
@@ -22,5 +25,6 @@ db.on('error', err => console.error)
 db.once('open', () => console.log('Connected to mongoose'))
 
 app.use('/', indexRouter)
+app.use('/authors', authorsRouter)
 
 app.listen(process.env.PORT || 3000)
